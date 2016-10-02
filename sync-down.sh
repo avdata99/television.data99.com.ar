30ed0be337675610c398541904c408c1679227aa
Andres Vazquez <andres@data99.com.ar>
2016-10-02 20:11:15 -0300

    ignoring

--- sync-down.sh
+++ sync-down.sh
@@ -5,4 +5,5 @@ rsync -av --progress \
 	--exclude="application/logs" \
 	--exclude="application/config/myconfig.php" \
 	--exclude="application/config/database.php" \
+	--exclude="application/views/views/" \
 	tele@television.data99.com.ar:/home/tele/www/ .
