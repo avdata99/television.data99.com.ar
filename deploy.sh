dbbd0692cc39e1050886d5cf542799b18a236edd
Andres Vazquez <andres@data99.com.ar>
2016-10-02 20:07:03 -0300

    nada

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@ rsync -av --progress \
 	--rsh='ssh -p 987' \
 	--exclude="error_log" \
 	--exclude="application/logs" \
+	--exclude="application/config/myconfig.php" \
+	--exclude="application/config/database.php" \
 	tele@television.data99.com.ar:/home/tele/www/ .
 
 #files
